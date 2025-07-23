6b0e0c363750ca76a88afd91dd99ab99cad327ea
Iosif Pravets <i@pravets.ru>
2025-07-23 11:55:38 +0000

    fix: исправлены замечания coderabbit

--- tests/test-yard.sh
+++ tests/test-yard.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 set -e
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 if [ -z "${CI-}" ]; then
   echo "The script is not running in CI"
-  SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
   source "${SCRIPT_DIR}/../.env"
 else
   echo "The script is running in CI"
 fi
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
 source "${SCRIPT_DIR}/../tools/assert.sh"
-
 
 test_yard_is_running() {
   log_header "Test :: yard is running"
@@ -22,7 +20,7 @@ test_yard_is_running() {
   expected="ИНФОРМАЦИЯ - [СписокРелизов1С]: Инициализирован обработчик"
   actual=$(docker run --rm ${DOCKER_REGISTRY_URL}/${DOCKER_LOGIN}/yard:latest 2>/dev/null | head -n1)
 
-  if assert_eq "$actual" "$expected"; then
+  if assert_eq "$expected" "$actual"; then
     log_success "yard is running test passed"
   else
     log_failure "yard is running test failed"
