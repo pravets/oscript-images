0832c1dcc6bf2cdb985ff1f5f0929ed2b9bf4981
Iosif Pravets <i@pravets.ru>
2025-07-24 08:06:53 +0000

    feat: реализована сборка образа загрузчика установщиков платформы и edt с сайта релизов

---
+++ src/build-onec-installer-downloader.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -euo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+if [ -z "${CI:-}" ]; then
+    echo "The script is not running in CI"
+    source "${SCRIPT_DIR}/../scripts/load_env.sh"	
+else
+    echo "The script is running in CI";
+fi
+
+source "${SCRIPT_DIR}/../scripts/docker_login.sh"
+source "${SCRIPT_DIR}/../tools/assert.sh"
+
+if [[ "${DOCKER_SYSTEM_PRUNE:-}" = "true" ]] ;
+then
+    docker system prune -af
+fi
+
+last_arg="."
+if [[ ${NO_CACHE:-} = "true" ]] ; then
+	last_arg="--no-cache ."
+fi
+
+onec_installer_downloader_version="${ONEC_INSTALLER_DOWNLOADER_VERSION}"
+image_name=onec-installer-downloader
+
+docker build \
+    --pull \
+    --build-arg ONEC_INSTALLER_DOWNLOADER_VERSION="${onec_installer_downloader_version}" \
+    --build-arg DOCKER_REGISTRY_URL="${DOCKER_REGISTRY_URL}" \
+    --build-arg DOCKER_LOGIN="${DOCKER_LOGIN}" \
+    -t "${DOCKER_REGISTRY_URL}/${DOCKER_LOGIN}/${image_name}:${onec_installer_downloader_version}" \
+    -f "${SCRIPT_DIR}/onec-installer-downloader/Dockerfile" \
+    ${last_arg} --debug
+
+if ./tests/test-onec-installer-downloader.sh; then  
+    docker push "${DOCKER_REGISTRY_URL}/${DOCKER_LOGIN}/${image_name}:${onec_installer_downloader_version}"
+
+    docker tag "${DOCKER_REGISTRY_URL}/${DOCKER_LOGIN}/${image_name}:${onec_installer_downloader_version}" "${DOCKER_REGISTRY_URL}/${DOCKER_LOGIN}/${image_name}:latest"
+    docker push "${DOCKER_REGISTRY_URL}/${DOCKER_LOGIN}/${image_name}:latest"
+
+    source "${SCRIPT_DIR}/../scripts/cleanup.sh"
+else
+    log_failure "ERROR: Тесты провалены. Образ не был запушен."
+    source "${SCRIPT_DIR}/../scripts/cleanup.sh"
+    exit 1
+fi
+exit 0
---
+++ tests/test-onec-installer-downloader.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+if [ -z "${CI-}" ]; then
+  echo "The script is not running in CI"
+  source "${SCRIPT_DIR}/../.env"
+else
+  echo "The script is running in CI"
+fi
+
+source "${SCRIPT_DIR}/../tools/assert.sh"
+
+test_onec_installer_downloader_is_running() {
+  log_header "Test :: ones-installer-downloader is running"
+
+
+  local expected actual
+
+  expected="Usage: /app/downloader.sh <installer_type> <ONEC_VERSION>"
+  actual=$(docker run --rm ${DOCKER_REGISTRY_URL}/${DOCKER_LOGIN}/onec-installer-downloader:${ONEC_INSTALLER_DOWNLOADER_VERSION} 2>/dev/null | head -n1)
+
+  if assert_eq "$expected" "$actual"; then
+    log_success "ones-installer-downloader is running test passed"
+  else
+    log_failure "ones-installer-downloader is running test failed"
+  fi
+}
+
+test_onec_installer_downloader_requires_credentials() {
+  log_header "Test :: ones-installer-downloader requires credentials"
+
+  local expected actual
+
+  expected="Ошибка: Необходимо установить переменные среды YARD_RELEASES_USER и YARD_RELEASES_PWD"
+  actual=$(docker run --rm ${DOCKER_REGISTRY_URL}/${DOCKER_LOGIN}/onec-installer-downloader:${ONEC_INSTALLER_DOWNLOADER_VERSION} thin-client32 8.3.25.1445 2>/dev/null | head -n1)
+
+  if assert_eq "$expected" "$actual"; then
+    log_success "ones-installer-downloader requires credentials test passed"
+  else
+    log_failure "ones-installer-downloader requires credentials test failed"
+  fi
+}
+
+# test calls
+test_onec_installer_downloader_is_running
+test_onec_installer_downloader_requires_credentials
